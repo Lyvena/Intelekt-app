06d6bfc81e3132bfd6f61ec41d1017d978c1116c
Intelekt Developer <dev@intelekt.com>
2025-12-30 21:39:04 +0530

    Fix collaboration export to use commentsService from proper file

--- frontend/src/components/collaboration/index.ts
+++ frontend/src/components/collaboration/index.ts
@@ -1,5 +1,6 @@
 export { CollaboratorCursors, CollaboratorBar } from './CollaboratorCursors';
-export { CodeCommentsPanel, CommentGutterIndicator, commentsService } from './CodeComments';
+export { CodeCommentsPanel, CommentGutterIndicator } from './CodeComments';
+export { commentsService } from './commentsService';
 export type { CodeComment } from './CodeComments';
 export { ProjectForkModal, PublicProjectsBrowser, ShareProjectButton } from './ProjectFork';
 export type { PublicProject } from './ProjectFork';
